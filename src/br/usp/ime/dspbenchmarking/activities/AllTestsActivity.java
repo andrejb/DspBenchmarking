01e20cbc7d3f79da9236c4438431440c7a5db8e5
Max <max@localhost.localdomain>
2013-08-02 14:43:14 -0300

    Airplane mode working

--- src/br/usp/ime/dspbenchmarking/activities/AllTestsActivity.java
+++ src/br/usp/ime/dspbenchmarking/activities/AllTestsActivity.java
@@ -827,8 +827,12 @@ public class AllTestsActivity extends Activity {
 	    	Resources res = getResources();
 	    	boolean preAPIv17 = res.getBoolean(R.bool.preAPI17);
 	    	if (preAPIv17) {
-	    		boolean isEnabled = Settings.System.getInt(getContentResolver(),Settings.System.AIRPLANE_MODE_ON, 0) == 1;
-	    		Settings.System.putInt(getContentResolver(), Settings.System.AIRPLANE_MODE_ON, isEnabled ? 0 : 1);    		
+	    		boolean isEnabled = (Settings.System.getInt(getContentResolver(),Settings.System.AIRPLANE_MODE_ON, 0) == 1);
+	    		Settings.System.putInt(getContentResolver(), Settings.System.AIRPLANE_MODE_ON, isEnabled ? 0 : 1);
+	    		
+	    		Intent intent = new Intent(Intent.ACTION_AIRPLANE_MODE_CHANGED);
+	    		intent.putExtra("state", !isEnabled);
+	    		sendBroadcast(intent);
 	    	}
 	    } catch (Exception e) {
 	        Toast.makeText(this, "exception:" + e.toString(), Toast.LENGTH_LONG).show();
